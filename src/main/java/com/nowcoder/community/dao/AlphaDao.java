886c4bc28b962df99bd3d8aff51bb71461cafec0
ccancle <821161464@qq.com>
2020-07-05 17:53:11 +0800

    first commit

---
+++ src/main/java/com/nowcoder/community/dao/AlphaDao.java
@@ -0,0 +1,12 @@
+package com.nowcoder.community.dao;
+
+/**
+ * @author ：ccancle菜菜
+ * @date ：Created in 2019/11/5 22:15
+ * @description：TODO
+ * @version: TODO
+ */
+public interface AlphaDao {
+    String select();
+
+}
